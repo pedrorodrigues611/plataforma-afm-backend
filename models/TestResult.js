105b085ea7ba729a6c16a8f607023895afc50a1c
pedro <hopertybnb@gmail.com>
2025-05-20 14:42:48 +0100

    Use VITE_API_URL para apontar ao backend em produção

--- models/TestResult.js
+++ models/TestResult.js
@@ -2,9 +2,10 @@
 const mongoose = require('mongoose');
 
 const testResultSchema = new mongoose.Schema({
-  userId:    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  points:    { type: Number, required: true },
-  createdAt: { type: Date, default: Date.now }
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  points: { type: Number, required: true },
+}, {
+  timestamps: true    // cria automaticamente createdAt e updatedAt
 });
 
 module.exports = mongoose.model('TestResult', testResultSchema);
