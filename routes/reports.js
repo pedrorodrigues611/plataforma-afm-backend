c39e2037ead10a86ccb6f637ee8869317afab431
pedro <hopertybnb@gmail.com>
2025-05-21 23:12:02 +0100

    Use VITE_API_URL para apontar ao backend em produção

--- routes/reports.js
+++ routes/reports.js
@@ -1,6 +1,7 @@
 // backend/routes/reports.js
 const express = require('express');
 const jwt     = require('jsonwebtoken');
+
 const Report  = require('../models/ReportedQuestion');
 const Pergunta = require('../models/Pergunta');
 const User     = require('../models/User');
